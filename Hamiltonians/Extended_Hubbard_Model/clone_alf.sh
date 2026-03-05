a0993ed6ea03e63a796c8d908c33a932bb42b2b3
Jonas Schwab <jonas.schwab@uni-wuerzburg.de>
2026-03-05 08:43:32 +0100

    Update links from GitLab to GitHub

--- Hamiltonians/Extended_Hubbard_Model/clone_alf.sh
+++ Hamiltonians/Extended_Hubbard_Model/clone_alf.sh
@@ -2,6 +2,6 @@
 BRANCH="master"
 HAMILTONIAN_NAME="Extended_Hubbard"
 
-git clone https://git.physik.uni-wuerzburg.de/ALF/ALF.git --branch $BRANCH || exit 1
+git clone https://github.com/ALF-QMC/ALF.git --branch $BRANCH || exit 1
 ln -s "$PWD/Hamiltonian_${HAMILTONIAN_NAME}_smod.F90" ALF/Prog/Hamiltonians || exit 1
 echo "$HAMILTONIAN_NAME" > ALF/Prog/Hamiltonians.list.d/Extended_Hubbard
