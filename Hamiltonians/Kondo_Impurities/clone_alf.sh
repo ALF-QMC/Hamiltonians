14dad9c94bf7b99b6171b268b12484bbee106f3f
Jonas Schwab <jonas.schwab@uni-wuerzburg.de>
2026-03-05 13:42:56 +0100

    Modify clone scripts to create shallow clones.

--- Hamiltonians/Kondo_Impurities/clone_alf.sh
+++ Hamiltonians/Kondo_Impurities/clone_alf.sh
@@ -2,7 +2,7 @@
 BRANCH="Base_ALF_for_Kondo_Impurities"
 HAMILTONIAN_NAME="Kondo_impurities"
 
-git clone https://github.com/ALF-QMC/ALF.git --branch $BRANCH || exit 1
+git clone --depth 1 https://github.com/ALF-QMC/ALF.git --branch $BRANCH || exit 1
 ln -s "$PWD/Hamiltonian_${HAMILTONIAN_NAME}_smod.F90" ALF/Prog/Hamiltonians || exit 1
-echo "$HAMILTONIAN_NAME" > ALF/Prog/Hamiltonians.list.d/Kondo_impurities
+echo "$HAMILTONIAN_NAME" > "ALF/Prog/Hamiltonians.list.d/$HAMILTONIAN_NAME"
 
